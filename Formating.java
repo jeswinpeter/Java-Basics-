68727e2f6f06d0d5ae818c03bebfbe5cd3170950
jeswinpeter <147077261+jeswinpeter@users.noreply.github.com>
2024-08-23 21:50:28 +0530

    Update Formating.java

--- Formating.java
+++ Formating.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Formating {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        
         System.out.println("================================");
-        for(int i=0;i<3;i++){
+        for(int i = 0;i < 3; i++){
             String inString = sc.next();
             int inInt = sc.nextInt();
             System.out.printf("%-15s%03d%n",inString,inInt);
