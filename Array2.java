c0923ba280e8f68460db0be43d4ed71c119a8e19
jeswinpeter <147077261+jeswinpeter@users.noreply.github.com>
2024-08-23 21:48:04 +0530

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -5,21 +5,21 @@ public class Array2 {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter size of the Array: ");
-        int n = scan.nextInt();
+        int size = scan.nextInt();
 
-        int a[] = new int[n];
+        int array[] = new int[n];
 
         System.out.println("Enter elements to the Array: ");
-        for(int j = 0; j < n; j++) {
-            a[j] = scan.nextInt();
+        for(int j = 0; j < size; j++) {
+            array[j] = scan.nextInt();
         }
         scan.close();
 
         // Prints each sequential element in array a
         System.out.print("Array : [");
-        for (int i = 0; i < n; i++) {
-            System.out.printf("%d",a[i]);
-            if(i < n-1) {
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%d",array[i]);
+            if(i < size-1) {
                 System.out.print(", ");
             }
         }
