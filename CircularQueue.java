ea2fd148c2721e1e4e3d6ce2cc21483cff4e72be
jeswinpeter <147077261+jeswinpeter@users.noreply.github.com>
2024-09-02 08:28:25 +0530

    W

--- CircularQueue.java
+++ CircularQueue.java
@@ -1,30 +1,42 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
-//int x = 5;
+class Functions{
+    int Max_size = 6;
+    int [] Queue = new int[Max_size];
+    int front = -1;
+    int rear = -1;
+    void enqueue(int value) {
+        if(front == -1 && rear == -1) {
+            front = 0;
+            rear = 0;
+            Queue[rear] = value;
+        }
 
-/*class Functions{
-    void enqueue(value) {
-        System.out.println("Can't enqueue -> The Queue is full!!!");
+        else if ((rear + 1)  % Max_size == front) {
+            System.out.println("Can't enqueue -> The Queue is full!!!");
+        }
+
+        else {
+            rear = (rear + 1) % Max_size;
+            Queue[rear] = value;
+        }
     }
-    
     void dequeue() {
         System.out.println("Can't dequeue -> The Queue is empty!!!");
     }
-    
-    void displa() {
-        System.out.println(----------Queue----------);
-        System.out.println(----------Queue----------);
+    void display() {
+        System.out.println("----------Queue----------");
+        System.out.println("----------Queue----------");
     }
-}*/
+}
 public class CircularQueue {
     public static void main(String[] args) {
-        //int option;
-        //int value;
+        Scanner scan = new Scanner(System.in);
+        int option;
+        int value;
+        int check = 1;
 
-        //System.out.println(x);
-
-        //Functions todo = new Functions();
-        //Scanner scan = new Scanner(System.in);
+        Functions todo = new Functions();
 
         //Menu
         System.out.println("The operations that can be performed on a Circular Queue are listed below :");
@@ -33,8 +45,35 @@ public class CircularQueue {
         System.out.println("3 -> Display");
         System.out.println("4 -> Exit");
 
+        while (check == 1) {
+            System.out.println("Enter function no. to be performed -> ");
+            option = scan.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Enter number to Enque -> ");
+                    value = scan.nextInt();
+                    todo.enqueue(value);
+                    break;
 
+                case 2:
+                    todo.dequeue();
+                    break;
 
+                case 3:
+                    todo.display();
+                    break;
+
+                case 4:
+                    System.out.println("==== Exiting!!! ====");
+                    check = 2;
+                    break;
+
+                default:
+                    System.out.println("Error!!!!\nEnter a valid input");
+
+            }
+
+        }
     }
 }
 
