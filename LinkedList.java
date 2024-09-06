4845de68d754b91735f2da9a358b40af5f6b59f9
jeswin <jeswinpeter92@gmail.com>
2024-09-06 23:17:51 +0530

    Coming soon (level 5)

---
+++ LinkedList.java
@@ -0,0 +1,7 @@
+//This is an upcoming project
+//Check again later
+public class LinkedList {
+    public static void main(String [] List) {
+        System.out.println("Thank you for checking out");
+    }
+}
