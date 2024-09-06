4a070ea0aa3d5c2238747ad926ff3e832cf03ea5
jeswin <jeswinpeter92@gmail.com>
2024-09-06 23:17:17 +0530

    Coming soon (level 3)

---
+++ Stack.java
@@ -0,0 +1,7 @@
+//This is an upcoming project
+//Check again later
+public class Stack {
+    public static void main(String [] Stack) {
+        System.out.println("Thank you for checking out");
+    }
+}
