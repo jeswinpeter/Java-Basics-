10387be2f814fa0ca18685e5e363e2b4cfd9ebf4
jeswin <jeswinpeter92@gmail.com>
2024-09-06 23:14:53 +0530

    Upcoming Code (Level Unknown)

---
+++ QuickSort.java
@@ -0,0 +1,7 @@
+//This is an upcoming project
+//Check again later
+public class QuickSort {
+    public static void main(String [] Quick) {
+        System.out.println("Thank you for checking out");
+    }
+}
