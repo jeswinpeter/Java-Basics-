084fc3c231501e70e3a7972ef497fe82b6557fc2
jeswin <jeswinpeter92@gmail.com>
2024-09-06 23:12:44 +0530

    Upcoming Code (Level 3)

---
+++ InsertionSort.java
@@ -0,0 +1,7 @@
+//This is an upcoming project
+//Check again later
+public class InsertionSort {
+    public static void main(String [] Insertion) {
+        System.out.println("Thank you for checking out");
+    }
+}
