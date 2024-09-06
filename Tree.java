dbc6d9cee04d6c47d05b643ee7bc647d4b5ae382
jeswin <jeswinpeter92@gmail.com>
2024-09-06 23:18:22 +0530

    Coming soon (level Unknown)

---
+++ Tree.java
@@ -0,0 +1,7 @@
+//This is an upcoming project
+//Check again later
+public class Tree {
+    public static void main(String [] Tree) {
+        System.out.println("Thank you for checking out");
+    }
+}
