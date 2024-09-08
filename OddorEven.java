0f483e30ee2212617ed3b202f8af863aec6596e0
jeswin <jeswinpeter92@gmail.com>
2024-09-08 08:55:30 +0530

    Rearranged (Level 00)

--- OddorEven.java
+++ OddorEven.java
@@ -1,29 +1,32 @@
-//This code checks if the number entered by the user is odd or even
+//This code checks if the number entered by the user is odd or even (Level 00)
 import java.util.Scanner;
 
 class Check {
     //Method to check if the input is odd or even
-    void inputCheck(int n){
-        if(n % 2 == 0){
-            System.out.print(n + " is even");
+    void inputCheck(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter any number: ");
+        int a = scan.nextInt();
+        scan.close();
+
+        if(a % 2 == 0){
+            System.out.print("---- " + n + " is even ----");
         }
         else {
-            System.out.print(n + " is odd");
+            System.out.print("---- " + n + " is odd ----");
         }
     }
 }
 
 public class OddorEven {
     public static void main(String[] args) {
-        
+
+        //Creating object for class Check
         Check toCheck = new Check();
-        Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Enter any number: ");
-        int a = scan.nextInt();
-        toCheck.inputCheck(a);
-        
-        scan.close();
+
+        //Calling inputCheck method
+        toCheck.inputCheck();
     }
 }
 
